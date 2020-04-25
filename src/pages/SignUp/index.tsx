766011399ee2a70095aaaca27879a46b4aeb7ed7
Iann Isacksson <iisackssonoliveira@gmail.com>
2020-04-25 19:26:04 -0300

    feat: Register Page

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FiArrowLeft, FiLogIn, FiMail, FiLock, FiUser } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={logoImg} alt="Logo" />
+      <form>
+        <h1>Faça seu cadastro</h1>
+
+        <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
+        <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
+
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Entrar</Button>
+
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+
+      <a href="login">
+        <FiArrowLeft />
+        Criar conta
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
