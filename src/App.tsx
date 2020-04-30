52038e1bee5e2ff2140fe7e94d54c360cbf82e83
Iann Isacksson <iisackssonoliveira@gmail.com>
2020-04-30 20:00:54 -0300

    feat: Hooks toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 
 import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+// import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
-import ToastContainer from './components/ToastComponent';
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks';
 
 const App: React.FC = () => {
   return (
     <>
-      <AuthProvider>
+      <AppProvider>
         <SignIn />
-      </AuthProvider>
-
-      <ToastContainer />
+      </AppProvider>
 
       <GlobalStyle />
     </>
