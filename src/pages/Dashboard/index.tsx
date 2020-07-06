e66b7dc13429fd59c054b43d18f90f4e1547c208
Iann <iisackssonoliveira@gmail.com>
2020-07-06 19:40:31 -0300

    feat: Styles next appointments

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextApointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -28,6 +37,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextApointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="http://localhost:3333/files/16fe56262fc3f7ad8c5f-0.jpeg"
+                alt="Iann Isacksson"
+              />
+
+              <strong>Iann Isacksson</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextApointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
