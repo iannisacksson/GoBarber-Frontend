035758b2ce317c81190c59b3f0357997abab0bf2
Iann <iisackssonoliveira@gmail.com>
2020-07-09 21:38:48 -0300

    feat: Schedule availability in the month

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -31,6 +31,8 @@ export const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBaber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       return { token, user: JSON.parse(user) };
     }
 
@@ -47,6 +49,8 @@ export const AuthProvider: React.FC = ({ children }) => {
 
     localStorage.setItem('@GoBaber:token', token);
     localStorage.setItem('@GoBaber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
